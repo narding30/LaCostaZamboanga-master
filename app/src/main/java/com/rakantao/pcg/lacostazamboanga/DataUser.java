89c289b77ffeeb8deec59e489976569aa6f8940b
abdulrahmanantao <ottoantao34@gmail.com>
2018-06-18 18:22:50 +0800

    added inspector module

---
+++ app/src/main/java/com/rakantao/pcg/lacostazamboanga/DataUser.java
@@ -0,0 +1,20 @@
+package com.rakantao.pcg.lacostazamboanga;
+
+public class DataUser {
+    public String Usertype;
+    public String email;
+    public String Password;
+    public String FirstName;
+    public String MiddleName;
+    public String LastName;
+
+    public DataUser(){
+
+    }
+
+    public DataUser(String user_type, String email, String password){
+        this.Usertype = user_type;
+        this.email = email;
+        this.Password = password;
+    }
+}
